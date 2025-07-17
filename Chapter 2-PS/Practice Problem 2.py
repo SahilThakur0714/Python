575e7400186e152c97fe43acc27c4e9869185e62
Sahil Thakur <sahilst0714@gmail.com>
2025-07-17 22:41:30 +0530

    Add files via upload

---
+++ Chapter 2-PS/Practice Problem 2.py
@@ -0,0 +1,6 @@
+# WAP to input side of a square & print its area
+
+side = float(input("Enter side of the square in cm: "))
+area = side**2
+
+print(f"Area of the square when side is {side} cm is {area} cm²")
---
+++ Chapter 2-PS/Practice Problem1.py
@@ -0,0 +1,7 @@
+# Write a Program to input 2 numbers & print their sum
+
+num1 = float(input("Enter a number1: "))
+num2 = float(input("Enter a number2: "))
+
+sum = num1 + num2
+print(f"Sum of {num1} & {num2} is {sum}")
